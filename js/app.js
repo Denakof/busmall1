93b69205d5310761725311fada7441582716e5d2
denakof <denakofahi_21@yahoo.com>
2021-05-18 18:20:28 +0300

    add

--- js/app.js
+++ js/app.js
@@ -3,6 +3,10 @@ let attempts = 0;
 let maxAttempts = 25;
 let attemptsEl = document.getElementById('attempts');
 let product = [];
+let productsNamesArray = [];
+let clicksArr = [];
+let viewsArr = [];
+let firstPicVal =[];
 function Busmall(productName) {
     //'cruisin-goat.jpg'.split('.') >>['cruisin-goat','jpg']
     this.productName = productName.split('.')[0];
@@ -10,6 +14,7 @@ function Busmall(productName) {
     this.clicks = 0;
     this.views = 0;
     product.push(this);
+    productsNamesArray.push(productName);
 }
 
 // let goat1 = new Busmall('cruisin-goat', 'images/cruisin-goat.jpg');
@@ -17,10 +22,10 @@ function Busmall(productName) {
 
 
 let productImages = ['bag.jpg', 'banana.jpg', 'bathroom.jpg',
-'boots.jpg', 'breakfast.jpg', 'bubblegum.jpg', 'chair.jpg',
-'cthulhu.jpg', 'dog-duck.jpg', 'dragon.jpg', 'pen.jpg', 'pet-sweep.jpg',
-'scissors.jpg', 'shark.jpg', 'sweep.png', 'tauntaun.jpg', 'unicorn.jpg', 'water-can.jpg',
-'wine-glass.jpg'];
+    'boots.jpg', 'breakfast.jpg', 'bubblegum.jpg', 'chair.jpg',
+    'cthulhu.jpg', 'dog-duck.jpg', 'dragon.jpg', 'pen.jpg', 'pet-sweep.jpg',
+    'scissors.jpg', 'shark.jpg', 'sweep.png', 'tauntaun.jpg', 'unicorn.jpg', 'water-can.jpg',
+    'wine-glass.jpg'];
 
 for (let i = 0; i < productImages.length; i++) {
     new Busmall(productImages[i]);
@@ -47,12 +52,16 @@ function renderImg() {
     rightImgIndex = generateImage();
     middleImgIndex = generateImage();
 
-
-    while (leftImgIndex === rightImgIndex || middleImgIndex === rightImgIndex || leftImgIndex === middleImgIndex ){
+console.log(firstPicVal);
+    while (leftImgIndex === rightImgIndex || middleImgIndex === rightImgIndex || leftImgIndex === middleImgIndex ||
+        // firstPicVal[0] === leftImgIndex || firstPicVal[1]=== leftImgIndex || firstPicVal[2]=== leftImgIndex ||
+        firstPicVal.includes(leftImgIndex)|| firstPicVal.includes(rightImgIndex)|| firstPicVal.includes(middleImgIndex) ) {
         leftImgIndex = generateImage();
         rightImgIndex = generateImage();
         middleImgIndex = generateImage();
     }
+    
+    firstPicVal= [leftImgIndex,rightImgIndex, middleImgIndex]
 
     lImgEl.setAttribute('src', product[leftImgIndex].source);
     lImgEl.setAttribute('title', product[leftImgIndex].source);
@@ -67,7 +76,7 @@ function renderImg() {
     product[middleImgIndex].views++;
 
     attemptsEl.textContent = attempts;
- 
+
 }
 renderImg();
 
@@ -79,42 +88,89 @@ mImgEl.addEventListener('click', handelClicks);
 function handelClicks(event) {
     attempts++;
     if (attempts <= maxAttempts) {
-        console.log(event.target.id)
+        // console.log(event.target.id)
         if (event.target.id === 'leftImg') {
             product[leftImgIndex].clicks++;
         } else if (event.target.id === 'rightImg') {
             product[rightImgIndex].clicks++;
         }
-        else if (event.target,id === 'middleImg'){
-        product[middleImgIndex].clicks++;}
+        else if (event.target. id === 'middleImg') {
+            product[middleImgIndex].clicks++;
+        }
         renderImg();
     } else {
-        let ulEl = document.getElementById('results');
-        let liEl;
-        for (let i = 0; i < product.length; i++) {
-            liEl = document.createElement('li');
-            ulEl.appendChild(liEl);
-            liEl.textContent = `${product[i].productName} has ${product[i].views} views and has ${product[i].clicks} clicks.`
+        // let ulEl = document.getElementById('results');
+        // let liEl;
         
-    }
+        for (let i = 0; i < product.length; i++) {
+            // liEl = document.createElement('li');
+            // ulEl.appendChild(liEl);
+            // liEl.textContent = `${product[i].productName} has ${product[i].views} views and has ${product[i].clicks} clicks.`
+            clicksArr.push(product[i].clicks);
+            viewsArr.push(product[i].views);
+        }
         lImgEl.removeEventListener('click', handelClicks);
         rImgEl.removeEventListener('click', handelClicks);
         mImgEl.removeEventListener('click', handelClicks);
-
+        ChartD();
     }
 
 }
-let button1 = getElementById('CLICKBUTTON');
+let button1 = document.getElementById('CLICKBUTTON');
 button1.addEventListener('click', BUTTONFUNCTION);
 
-function BUTTONFUNCTION (){
+function BUTTONFUNCTION() {
     let ulEl = document.getElementById('results');
     let liEl;
     for (let i = 0; i < product.length; i++) {
         liEl = document.createElement('li');
         ulEl.appendChild(liEl);
         liEl.textContent = `${product[i].productName} has ${product[i].views} views and has ${product[i].clicks} clicks.`
-    } 
+
+    }
     button1.removeEventListener('click', BUTTONFUNCTION);
 
+}
+function ChartD() {
+    var ctx = document.getElementById('myChart').getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: productsNamesArray,
+            datasets: [{
+                label: '# of clicks',
+                data: clicksArr,
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+
+                ],
+                borderColor: [
+
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }, {
+                label: '# of views',
+                data: viewsArr,
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+
+                ],
+                borderColor: [
+
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }
+
+            ]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
 }
